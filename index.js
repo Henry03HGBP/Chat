1640c04364bbc0ac74b5012581788fc2e764d9be
Henry03HGBP <henry.bautista.prado@icloud.com>
2026-05-17 15:49:01 -0600

    servidor montado

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const http = require('http');
+
 const express = require('express');
+const socketio = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
+
+
+// Enviando Archivos Estáticos
 app.use(express.static('public'));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+// Inicializando Servidor 
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
-
